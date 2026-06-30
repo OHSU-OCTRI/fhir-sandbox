193eda726467ff6774d516c0253dda92968f52f2
Amy Yates <yateam@ohsu.edu>
2026-06-30 08:24:14 -0700

    RFS-307: Clean up standalone-launch.ts

--- src/main/resources/frontend/standalone-launch.ts
+++ src/main/resources/frontend/standalone-launch.ts
@@ -5,19 +5,13 @@ import StandaloneLauncher from './components/StandaloneLauncher.vue';
 
 const root = document.getElementById('standalone-launcher-app');
 if (root) {
-  const csrfToken = getCsrfToken() ?? '';
-
-  const ctxMeta = document.querySelector<HTMLMetaElement>('meta[name="ctx"]');
-  const ctx = ctxMeta?.content?.replace(/\/$/, '') ?? '';
-  const completeUrl = `${window.location.origin}${ctx}/smart/standalone-launch/complete`;
-
   const app = createApp(StandaloneLauncher, {
-    fhirApi: root.dataset.fhirApi ?? '',
-    accessToken: root.dataset.accessToken ?? '',
-    clientId: root.dataset.clientId ?? '',
-    sessionKey: root.dataset.sessionKey ?? '',
-    csrfToken,
-    completeUrl,
+    fhirApi: root?.dataset.fhirApi,
+    accessToken: root?.dataset.accessToken,
+    clientId: root?.dataset.clientId,
+    sessionKey: root?.dataset.sessionKey,
+    csrfToken: getCsrfToken(),
+    completeUrl: root?.dataset.completeUrl,
   });
   app.mount('#standalone-launcher-app');
 } else {
